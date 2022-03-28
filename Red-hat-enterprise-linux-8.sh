d5cd4bb6e1949c9eeaf8fbf34ab0d89288a75aad
root <cogalyunus@gmail.com>
2022-03-28 17:45:23 +0300

    Gitea option has been added

--- Red-hat-enterprise-linux-8.sh
+++ Red-hat-enterprise-linux-8.sh
@@ -31,7 +31,7 @@ options=("PHP ${opts[1]}" "Grub Customizer ${opts[2]}" "Python ${opts[3]}" "Wine
 "VLC ${opts[38]}" "UFW ${opts[39]}" "Fail2ban ${opts[40]}" "Google Authenticator ${opts[41]}" "Composer ${opts[42]}" 
 "Podman ${opts[43]}" "NFS Server ${opts[44]}" "Elasticsearch ${opts[45]}" "Kibana ${opts[46]}"
 "pgAdmin ${opts[47]}" "pgAgent ${opts[48]}" "Zabbix Agent ${opts[49]}" "Enterprise Search ${opts[50]}" 
-"Logstash ${opts[51]}" "Done ${opts[52]}")
+"Logstash ${opts[51]}" "Gitea ${opts[52]}" "Done ${opts[53]}")
     select opt in "${options[@]}"
     do
         case $opt in
@@ -239,10 +239,14 @@ options=("PHP ${opts[1]}" "Grub Customizer ${opts[2]}" "Python ${opts[3]}" "Wine
                 choice 51
                 break
                 ;;
-            "Done ${opts[52]}")
+            "Gitea ${opts[52]}")
+                choice 52
+                break
+                ;;
+            "Done ${opts[53]}")
                 break 2
                 ;;
-            *) printf '%s\n' 'Please Choose Between 1-52';;
+            *) printf '%s\n' 'Please Choose Between 1-53';;
         esac
     done
 done
@@ -277,6 +281,7 @@ printf "\n"
 #Necessary Packages
 #sudo dnf -vy update
 sudo subscription-manager repos --enable codeready-builder-for-rhel-8-"$(arch)"-rpms
+sudo subscription-manager repos --enable "rhel-*-optional-rpms" --enable "rhel-*-extras-rpms"
 sudo dnf -vy install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo dnf -vy install yum-utils dnf-utils
 sudo dnf -vy install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm \
@@ -2962,10 +2967,10 @@ else
 fi
 ;;
 
-50)
+51)
 #Logstash
 printf "\nPlease Choose Your Desired Logstash Version\n\n1-) Logstash (From Official Package)\n\
-2-) Logstash (Via Docker)\n3-) Logstash (From .rpm file)\nPlease Select Your Enterprise Search Version:"
+2-) Logstash (Via Docker)\n3-) Logstash (From .rpm file)\nPlease Select Your Logstash Version:"
 read -r logstash_version
 if [ "$logstash_version" = "1" ];then
     sudo dnf -vy rmeove java*
@@ -3013,6 +3018,90 @@ elif [ "$logstash_version" = "4" ];then
     https://artifacts.elastic.co/downloads/logstash/logstash-8.1.0-linux-x86_64.tar.gz
     tar -xvf /root/Downloads/logstash-8.1.0-linux-x86_64.tar.gz -C /root/Downloads/logstash --strip-components 1
     echo "Installation completed, folder under /root/Downloads/logstash/"
+else
+    echo "Out of options please choose between 1-3"
+fi
+;;
+
+52)
+#Gitea
+printf "\nPlease Choose Your Desired Gitea Version\n\n1-) Gitea (Install from binary)\n\
+2-) Gitea (Install via snap)\n3-) Gitea (Via Docker)\n\nPlease Select Your Gitea Version:"
+read -r gitea_version
+if [ "$gitea_version" = "1" ];then
+    gitea_stable_link=$(lynx -dump https://dl.gitea.io/gitea/ | awk '/http/ {print $2}' | grep -iv "rc\|dev\|.json" \
+    | grep -i "gitea/" | head -n 1)
+    gitea_stable_link=$(lynx -dump "$gitea_stable_link" | awk '/http/ {print $2}' | grep -iv ".exe\|sha\|asc\|darwin\|xz" \
+    | grep -i linux-amd64 |  head -n 1)
+    sudo mkdir -pv /root/Downloads/gitea
+    wget -O /root/Downloads/gitea/gitea-stable-linux-amd64 "$gitea_stable_link"
+    sudo chmod +x /root/Downloads/gitea/gitea-stable-linux-amd64
+    sudo dnf -vy install git
+    sudo useradd \
+    --system \
+    --shell /bin/bash \
+    --comment 'Git Version Control' \
+    --create-home \
+    --home /home/git \
+    git
+    sudo mv -v /root/Downloads/gitea/gitea-stable-linux-amd64 /usr/local/bin/gitea
+    mkdir -p /var/lib/gitea/{custom,data,log}
+    chown -R git:git /var/lib/gitea/
+    chmod -R 750 /var/lib/gitea/
+    mkdir /etc/gitea
+    chown root:git /etc/gitea
+    chmod 770 /etc/gitea
+    sudo wget https://raw.githubusercontent.com/go-gitea/gitea/master/contrib/systemd/gitea.service -P /etc/systemd/system/
+    sudo systemctl daemon-reload
+    sudo systemctl enable --now gitea
+    sudo systemctl start gitea
+    sudo dnf -vy install nginx
+    echo "server {
+    listen 80;
+    server_name gitea;
+
+    location / {
+        proxy_pass http://localhost:3000;
+    }
+}" > /etc/nginx/conf.d/gitea.conf
+    sudo systemctl restart nginx
+elif [ "$gitea_version" = "2" ];then
+    sudo snap install gitea
+elif [ "$gitea_version" = "3" ];then
+    #Docker
+    sudo dnf -vy install yum-utils
+    sudo dnf -vy install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+    sudo yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
+    sudo dnf -vy install docker-ce --nobest docker-ce-cli containerd.io
+    systemctl start docker
+    systemctl enable docker
+    sudo mkdir -pv /root/gitea/
+    echo "version: "3"
+
+networks:
+  gitea:
+    external: false
+
+services:
+  server:
+    image: gitea/gitea:latest
+    container_name: gitea
+    environment:
+      - USER_UID=1000
+      - USER_GID=1000
+    restart: always
+    networks:
+      - gitea
+    volumes:
+      - ./gitea:/data
+      - /etc/timezone:/etc/timezone:ro
+      - /etc/localtime:/etc/localtime:ro
+    ports:
+      - "3000:3000"
+      - "222:22"" > /root/gitea/docker-compose.yml
+    
 else
     echo "Out of options please choose between 1-3"
 fi
